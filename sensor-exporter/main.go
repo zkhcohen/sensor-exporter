49c9f809e1e1210a88e7d02458c8d2b675808de2
zkhcohen <zacharykhcohen@gmail.com>
2022-02-24 23:50:06 -0800

    Added pwrstat exporter.

--- sensor-exporter/main.go
+++ sensor-exporter/main.go
@@ -12,9 +12,10 @@ import (
 	"strconv"
 	"strings"
 
+	"github.com/kerwenwwer/gopwrstat"
 	"github.com/md14454/gosensors"
 	"github.com/prometheus/client_golang/prometheus"
-    	"github.com/prometheus/client_golang/prometheus/promhttp"
+    "github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 var (
@@ -47,39 +48,49 @@ var (
 		"temperature in celsius",
 		[]string{"device", "id"},
 		nil)
+
+	LoadDesc = prometheus.NewDesc(
+		"ups_load",
+		"UPS power load (Watt)",
+		[]string{"device"},
+		nil)
+
+	StateDesc = prometheus.NewDesc(
+		"ups_state",
+		"UPS status (1 -> Normal, 0 -> Not)",
+		[]string{"device"},
+		nil)
+
+	BatteryDesc = prometheus.NewDesc(
+		"ups_battery_capacity",
+		"UPS battery capacity(%)",
+		[]string{"device"},
+		nil)
+
+	RtimeDesc = prometheus.NewDesc(
+		"ups_remaining_runtime",
+		"UPS Remaining Runtime(min)",
+		[]string{"device"},
+		nil)
+
+	InVoltageDesc = prometheus.NewDesc(
+		"ups_in_voltage",
+		"UPS Input Voltage(V): pass-> 1 non_pass -> 0",
+		[]string{"device"},
+		nil)
+
+	OutVoltageDesc = prometheus.NewDesc(
+		"ups_out_voltage",
+		"UPS Output Voltage(V): pass-> 1 non_pass -> 0",
+		[]string{"device"},
+		nil)
+
+	TestDesc = prometheus.NewDesc(
+		"ups_test_result",
+		"UPS Test Result",
+		[]string{"device"},
+		nil)
 )
-
-func main() {
-	var (
-		listenAddress  = flag.String("web.listen-address", ":9255", "Address on which to expose metrics and web interface.")
-		metricsPath    = flag.String("web.telemetry-path", "/metrics", "Path under which to expose metrics.")
-		hddtempAddress = flag.String("hddtemp-address", "10.0.10.6:7634", "Address to fetch hdd metrics from.")
-	)
-	flag.Parse()
-
-	hddcollector := NewHddCollector(*hddtempAddress)
-	if err := hddcollector.Init(); err != nil {
-		log.Printf("error readding hddtemps: %v", err)
-	}
-	prometheus.MustRegister(hddcollector)
-
-	lmscollector := NewLmSensorsCollector()
-	lmscollector.Init()
-	prometheus.MustRegister(lmscollector)
-
-	http.Handle(*metricsPath, promhttp.Handler())
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`<html>
-			<head><title>Sensor Exporter</title></head>
-			<body>
-			<h1>Sensor Exporter</h1>
-			<p><a href="` + *metricsPath + `">Metrics</a></p>
-			</body>
-			</html>`))
-	})
-	http.ListenAndServe(*listenAddress, nil)
-}
 
 type (
 	LmSensorsCollector struct{}
@@ -245,4 +256,114 @@ func (h *HddCollector) Collect(ch chan<- prometheus.Metric) {
 			ht.Device,
 			ht.Id)
 	}
+}
+
+type (
+	PwrstatCollector struct{}
+)
+
+func NewPwrstatCollector() *PwrstatCollector {
+	return &PwrstatCollector{}
+}
+
+func (l *PwrstatCollector) Describe(ch chan<- *prometheus.Desc) {
+	ch <- LoadDesc
+	ch <- StateDesc
+	ch <- BatteryDesc
+	ch <- RtimeDesc
+	ch <- OutVoltageDesc
+	ch <- TestDesc
+}
+
+func (l *PwrstatCollector) Collect(ch chan<- prometheus.Metric) {
+	status, err := gopwrstat.NewFromSystem()
+	if err != nil {
+		panic(err)
+	}
+	for k, v := range status.Status {
+
+		if k == "Load" {
+			value_arr := strings.Fields(v)
+			if value, err := strconv.ParseFloat(value_arr[0], 64); err == nil {
+				ch <- prometheus.MustNewConstMetric(LoadDesc,
+					prometheus.GaugeValue, value, status.Status["Model Name"])
+			}
+		} else if k == "State" {
+			var state = 0
+			if v == "Normal" {
+				state = 1
+			}
+			ch <- prometheus.MustNewConstMetric(StateDesc,
+				prometheus.GaugeValue, float64(state), status.Status["Model Name"])
+		} else if k == "Battery Capacity" {
+			value_arr := strings.Fields(v)
+			if value, err := strconv.ParseFloat(value_arr[0], 64); err == nil {
+				ch <- prometheus.MustNewConstMetric(BatteryDesc,
+					prometheus.GaugeValue, value, status.Status["Model Name"])
+			}
+		} else if k == "Remaining Runtime" {
+			value_arr := strings.Fields(v)
+			if value, err := strconv.ParseFloat(value_arr[0], 64); err == nil {
+				ch <- prometheus.MustNewConstMetric(RtimeDesc,
+					prometheus.GaugeValue, value, status.Status["Model Name"])
+			}
+		} else if k == "Utility Voltage" {
+			value_arr := strings.Fields(v)
+			if value, err := strconv.ParseFloat(value_arr[0], 64); err == nil {
+				ch <- prometheus.MustNewConstMetric(InVoltageDesc,
+					prometheus.GaugeValue, value, status.Status["Model Name"])
+			}
+		} else if k == "Output Voltage" {
+			value_arr := strings.Fields(v)
+			if value, err := strconv.ParseFloat(value_arr[0], 64); err == nil {
+				ch <- prometheus.MustNewConstMetric(OutVoltageDesc,
+					prometheus.GaugeValue, value, status.Status["Model Name"])
+			}
+		} else if k == "Test Result" {
+			value_arr := strings.Fields(v)
+			if value_arr[0] == "Passed" {
+				ch <- prometheus.MustNewConstMetric(TestDesc,
+					prometheus.GaugeValue, 1, status.Status["Model Name"])
+			} else {
+				ch <- prometheus.MustNewConstMetric(TestDesc,
+					prometheus.GaugeValue, 0, status.Status["Model Name"])
+			}
+		}
+	}
+}
+
+
+func main() {
+	var (
+		listenAddress  = flag.String("web.listen-address", ":9255", "Address on which to expose metrics and web interface.")
+		metricsPath    = flag.String("web.telemetry-path", "/metrics", "Path under which to expose metrics.")
+		hddtempAddress = flag.String("hddtemp-address", "10.0.10.6:7634", "Address to fetch hdd metrics from.")
+	)
+	flag.Parse()
+
+	hddcollector := NewHddCollector(*hddtempAddress)
+	if err := hddcollector.Init(); err != nil {
+		log.Printf("error readding hddtemps: %v", err)
+	}
+	prometheus.MustRegister(hddcollector)
+
+	lmscollector := NewLmSensorsCollector()
+	lmscollector.Init()
+	prometheus.MustRegister(lmscollector)
+
+	pwrstatCollector := NewPwrstatCollector()
+	prometheus.MustRegister(pwrstatCollector)
+
+	http.Handle(*metricsPath, promhttp.Handler())
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`<html>
+			<head><title>Sensor Exporter</title></head>
+			<body>
+			<h1>Sensor Exporter</h1>
+			<p><a href="` + *metricsPath + `">Metrics</a></p>
+			</body>
+			</html>`))
+	})
+	http.ListenAndServe(*listenAddress, nil)
 }
